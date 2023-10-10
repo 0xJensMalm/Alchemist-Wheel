1e6448a31404c4c84e4967e73d6401557e869cc7
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2023-10-10 21:52:34 +0200

    fixed tooltip - added elementinfo.json

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
+let elementData;
+// Load JSON data
+d3.json("elementInfo.json").then(data => {
+  elementData = data;
+});
 document.addEventListener("DOMContentLoaded", function() {
+  
   const elements = ['Hydrogen', 'Oxygen', 'Carbon', 'Nitrogen', 'Sodium', 'Chlorine', 'Phosphorus', 'Calcium', 'Silicon', 'Iron'];
   const symbols = ['H', 'O', 'C', 'N', 'Na', 'Cl', 'P', 'Ca', 'Si', 'Fe'];
   const colors = ['#6F2DBD', '#2C7A7B', '#2E294E', '#AB83A1', '#D81E5B', '#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#073B4C'];
@@ -9,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const innerRadius = 120;
   
   const infoFrame = d3.select("#element-title");
+  
   
   const svg = d3.select("#wheel-container")
     .append("svg")
@@ -34,9 +41,25 @@ document.addEventListener("DOMContentLoaded", function() {
     .attr("stroke", "#ffffff")
     .attr("stroke-width", "1")
     .on("mouseover", function(event, d, i) {
-      infoFrame.text(`${elements[d.index]} (${symbols[d.index]})`);
-    })
-    .on("mouseout", function(d) {
-      infoFrame.text("Hover over an element");
-    });
+      // Keep the existing highlighting effect
+      d3.select(this)
+        .attr("stroke", "#000000")
+        .attr("stroke-width", "2");
+
+    // Add the new tooltip behavior
+    const elementInfo = elementData.find(e => e.symbol === symbols[d.index]);
+    if (elementInfo) {
+      infoFrame.html(`<h1>${elementInfo.name} (${elementInfo.symbol})</h1><p>${elementInfo.description}</p>`);
+    }
+  })
+  .on("mouseout", function(d) {
+    // Reset the highlighting effect
+    d3.select(this)
+      .attr("stroke", "#ffffff")
+      .attr("stroke-width", "1");
+
+    // Reset the tooltip
+    infoFrame.html("<h1>Hover over an element</h1>");
+  });
+  
 });
