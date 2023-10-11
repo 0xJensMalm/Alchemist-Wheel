540360c59fe02d0fbabdbd9a18795b3b00d4bc2b
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2023-10-11 09:38:24 +0200

    fixed curved text

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let elementData;
+
 // Load JSON data
 d3.json("elementInfo.json").then(data => {
   elementData = data;
 });
+
 document.addEventListener("DOMContentLoaded", function() {
   
   const elements = ['Hydrogen', 'Oxygen', 'Carbon', 'Nitrogen', 'Sodium', 'Chlorine', 'Phosphorus', 'Calcium', 'Silicon', 'Iron'];
@@ -13,8 +15,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const height = 600;
   const outerRadius = 210;
   const innerRadius = 170;
-  
-  
 
   const infoFrame = d3.select("#element-title");
 
@@ -32,7 +32,8 @@ document.addEventListener("DOMContentLoaded", function() {
   const pie = d3.pie()
     .value(1)
     .sort(null);
-  
+
+  // Wheel Sections
   const path = svg.selectAll("path")
     .data(pie(symbols))
     .enter()
@@ -42,51 +43,44 @@ document.addEventListener("DOMContentLoaded", function() {
     .attr("stroke", "#ffffff")
     .attr("stroke-width", "1")
     .on("mouseover", function(event, d, i) {
-      // Keep the existing highlighting effect
       d3.select(this)
         .attr("stroke", "#000000")
         .attr("stroke-width", "2");
 
+      const elementInfo = elementData.find(e => e.symbol === symbols[d.index]);
+      if (elementInfo) {
+        infoFrame.html(`<h1>${elementInfo.name} (${elementInfo.symbol})</h1><p>${elementInfo.description}</p>`);
+      }
+    })
+    .on("mouseout", function(d) {
+      d3.select(this)
+        .attr("stroke", "#ffffff")
+        .attr("stroke-width", "1");
+      infoFrame.html("<h1>Hover over an element</h1>");
+    });
+
+  // Text Arcs and Labels
   const textArc = d3.arc()
-  .innerRadius(innerRadius - 25)  // 10 units smaller than the wheel's innerRadius
-  .outerRadius(outerRadius - 25);  // 10 units smaller than the wheel's outerRadius
+    .innerRadius(innerRadius - 25)
+    .outerRadius(outerRadius - 25);
 
   const textArcs = svg.selectAll(".textArc")
-  .data(pie(symbols))
-  .enter().append("path")
-  .attr("id", (d, i) => `textArc${i}`)
-  .attr("d", arc)
-  .attr("d", textArc)
-  .style("fill", "none")
-  .style("stroke", "none");
+    .data(pie(symbols))
+    .enter().append("path")
+    .attr("id", (d, i) => `textArc${i}`)
+    .attr("d", textArc)
+    .style("fill", "none")
+    .style("stroke", "none");
 
-// Add labels that follow the text arcs
-const labels = svg.selectAll(".arcLabel")
-  .data(pie(symbols))
-  .enter().append("text")
-  .attr("class", "arcLabel");
+  const labels = svg.selectAll(".arcLabel")
+    .data(pie(symbols))
+    .enter().append("text")
+    .attr("class", "arcLabel");
 
-labels.append("textPath")
-  .attr("xlink:href", (d, i) => `#textArc${i}`)
-  .attr("startOffset", "20%")  // Adjust this to move the text closer to the center
-  .attr("dy", "-15em")  // Adjust this to move the text up or down
-  .style("text-anchor", "middle")
-  .text((d, i) => elements[i]);
-
-    // Add the new tooltip behavior
-    const elementInfo = elementData.find(e => e.symbol === symbols[d.index]);
-    if (elementInfo) {
-      infoFrame.html(`<h1>${elementInfo.name} (${elementInfo.symbol})</h1><p>${elementInfo.description}</p>`);
-    }
-  })
-  .on("mouseout", function(d) {
-    // Reset the highlighting effect
-    d3.select(this)
-      .attr("stroke", "#ffffff")
-      .attr("stroke-width", "1");
-
-    // Reset the tooltip
-    infoFrame.html("<h1>Hover over an element</h1>");
-  });
-  
+  labels.append("textPath")
+    .attr("xlink:href", (d, i) => `#textArc${i}`)
+    .attr("startOffset", "20%")
+    .attr("dy", "-15em")
+    .style("text-anchor", "middle")
+    .text((d, i) => elements[i]);
 });
