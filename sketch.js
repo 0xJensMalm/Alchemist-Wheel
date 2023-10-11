cfc30c5fa3bf21bdc7e2d70535d61daa75bb2a12
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2023-10-11 09:14:13 +0200

    updated

--- sketch.js
+++ sketch.js
@@ -7,16 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
   
   const elements = ['Hydrogen', 'Oxygen', 'Carbon', 'Nitrogen', 'Sodium', 'Chlorine', 'Phosphorus', 'Calcium', 'Silicon', 'Iron'];
   const symbols = ['H', 'O', 'C', 'N', 'Na', 'Cl', 'P', 'Ca', 'Si', 'Fe'];
-  const colors = ['#6F2DBD', '#2C7A7B', '#2E294E', '#AB83A1', '#D81E5B', '#EF476F', '#FFD166', '#06D6A0', '#118AB2', '#073B4C'];
+  const colors = ['#ADD8E6', '#87CEEB', '#333333', '#D8BFD8', '#FFA500', '#008000', '#FF0000', '#D3D3D3', '#8B4513', '#B7410E'];
   
-  const width = 400;
-  const height = 400;
-  const outerRadius = 140;
-  const innerRadius = 120;
+  const width = 600;
+  const height = 600;
+  const outerRadius = 210;
+  const innerRadius = 170;
   
+  
+
   const infoFrame = d3.select("#element-title");
-  
-  
+
   const svg = d3.select("#wheel-container")
     .append("svg")
     .attr("width", width)
@@ -45,6 +46,32 @@ document.addEventListener("DOMContentLoaded", function() {
       d3.select(this)
         .attr("stroke", "#000000")
         .attr("stroke-width", "2");
+
+  const textArc = d3.arc()
+  .innerRadius(innerRadius - 25)  // 10 units smaller than the wheel's innerRadius
+  .outerRadius(outerRadius - 25);  // 10 units smaller than the wheel's outerRadius
+
+  const textArcs = svg.selectAll(".textArc")
+  .data(pie(symbols))
+  .enter().append("path")
+  .attr("id", (d, i) => `textArc${i}`)
+  .attr("d", arc)
+  .attr("d", textArc)
+  .style("fill", "none")
+  .style("stroke", "none");
+
+// Add labels that follow the text arcs
+const labels = svg.selectAll(".arcLabel")
+  .data(pie(symbols))
+  .enter().append("text")
+  .attr("class", "arcLabel");
+
+labels.append("textPath")
+  .attr("xlink:href", (d, i) => `#textArc${i}`)
+  .attr("startOffset", "20%")  // Adjust this to move the text closer to the center
+  .attr("dy", "-15em")  // Adjust this to move the text up or down
+  .style("text-anchor", "middle")
+  .text((d, i) => elements[i]);
 
     // Add the new tooltip behavior
     const elementInfo = elementData.find(e => e.symbol === symbols[d.index]);
